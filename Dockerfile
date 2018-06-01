b1890e2273142cf173f6766f3fccd65b7fcd6a28
David Huie <dahuie@gmail.com>
2018-05-31 19:57:42 -0700

    Hacky install @ boot

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:18.04
+FROM ubuntu:16.04
 
 # Thanks to https://nbsoftsolutions.com/blog/routing-select-docker-containers-through-wireguard-vpn
 RUN apt-get update -y && \
@@ -6,8 +6,7 @@ RUN apt-get update -y && \
     echo resolvconf resolvconf/linkify-resolvconf boolean false | debconf-set-selections && \
     echo "REPORT_ABSENT_SYMLINK=no" >> /etc/default/resolvconf && \
     add-apt-repository --yes ppa:wireguard/wireguard && \
-    apt-get update -y && \
-    apt-get install -y wireguard resolvconf
+    apt-get install resolvconf
 
 COPY scripts /scripts
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-interfaces=`find /etc/wireguard -type f`
+# Install Wireguard. This has to be done dynamically since the kernel
+# module depends on the host kernel version.
+apt update
+apt install -y linux-headers-$(uname -r)
+apt install -y wireguard
 
+# Find a Wireguard interface
+interfaces=`find /etc/wireguard -type f`
 if [[ -z $interfaces ]]; then
 	echo "$(date): Interface not found in /etc/wireguard" >&2
 	exit 1
@@ -14,6 +20,7 @@ interface=`echo $interfaces | head -n 1`
 echo "$(date): Starting Wireguard"
 wg-quick up $interface
 
+# Handle shutdown behavior
 finish () {
     echo "$(date): Shutting down Wireguard"
     wg-quick down $interface
