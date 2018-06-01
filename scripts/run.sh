e8966ae6c5843543baae4445d8eef44f5e83e42e
David Huie <dahuie@gmail.com>
2018-05-31 22:18:10 -0700

    Fix whitespace

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,8 +11,8 @@ apt install -y wireguard
 # Find a Wireguard interface
 interfaces=`find /etc/wireguard -type f`
 if [[ -z $interfaces ]]; then
-	echo "$(date): Interface not found in /etc/wireguard" >&2
-	exit 1
+    echo "$(date): Interface not found in /etc/wireguard" >&2
+    exit 1
 fi
 
 interface=`echo $interfaces | head -n 1`
@@ -24,7 +24,7 @@ wg-quick up $interface
 finish () {
     echo "$(date): Shutting down Wireguard"
     wg-quick down $interface
-	exit 0
+    exit 0
 }
 
 trap finish SIGTERM SIGINT SIGQUIT
